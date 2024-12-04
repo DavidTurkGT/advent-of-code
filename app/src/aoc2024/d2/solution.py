5d0d6bff8a0b08f23e3a96b6b8588f6524c60281
David Turk <david.turk@cfacorp.com>
2024-12-04 12:45:14 -0500

    Completes day 2 part 2

--- app/src/aoc2024/d2/solution.py
+++ app/src/aoc2024/d2/solution.py
@@ -1,13 +1,19 @@
 def main() -> None:
     reports = read_input_file()
-    safe_reports = list(filter(determine_report_safe, reports))
+    unsafe_reports = list(filter(lambda x: not determine_report_safe(x), reports))
 
-    print(f"There are {len(safe_reports)} safe reports")
+    print(f"There are {len(reports) - len(unsafe_reports)} safe reports")
 
-    safe_reports_with_dampener = list(
-        filter(lambda x: determine_report_safe(x, problem_dampener=True), reports)
+    safe_reports_after_dampening = list(
+        filter(determine_safe_after_dampening, unsafe_reports)
     )
-    print(f"There are {len(safe_reports_with_dampener)} safe reports after dampening")
+
+    print(
+        f"There are {len(safe_reports_after_dampening)} that are now safe after dampening"
+    )
+    print(
+        f"There are {len(reports) - len(unsafe_reports) + len(safe_reports_after_dampening)} total safe reports now"
+    )
 
 
 def read_input_file() -> list[list[int]]:
@@ -21,23 +27,29 @@ def read_input_file() -> list[list[int]]:
     return report_list
 
 
-def determine_report_safe(
-    levels: list[int], problem_dampener=False, rerun=False
-) -> bool:
-    if rerun:
-        print(f"Looking at {levels}")
+def determine_report_safe(report: list[int]) -> bool:
     # Determine if the list should be increasing or decreasing
-    if levels[0] > levels[1]:
+    if report[0] > report[1]:
         should_increase = False
     else:
         should_increase = True
 
-    for a, b in zip(levels, levels[1:]):
+    for a, b in zip(report, report[1:]):
         if (should_increase and b < a) or (not should_increase and b > a):
             return False
         if abs(a - b) > 3 or abs(a - b) < 1:
             return False
     return True
+
+
+def determine_safe_after_dampening(unsafe_report: list[int]) -> bool:
+    for index, _ in enumerate(unsafe_report):
+        # Attempt to remove the element at the index. If the report is now safe, then
+        # return True
+        if determine_report_safe(unsafe_report[:index] + unsafe_report[index + 1 :]):
+            return True
+
+    return False
 
 
 if __name__ == "__main__":
--- app/test/aoc2024/test_2024d2_solution.py
+++ app/test/aoc2024/test_2024d2_solution.py
@@ -1,14 +1,18 @@
-from src.aoc2024.d2.solution import determine_report_safe
+from src.aoc2024.d2.solution import (
+    determine_report_safe,
+    determine_safe_after_dampening,
+)
+
+report_1 = [7, 6, 4, 2, 1]
+report_2 = [1, 2, 7, 8, 9]
+report_3 = [9, 7, 6, 2, 1]
+report_4 = [1, 3, 2, 4, 5]
+report_5 = [8, 6, 4, 4, 1]
+report_6 = [1, 3, 6, 7, 9]
+report_7 = [6, 4, 7, 8, 10, 11]
 
 
 def test_determine_report_safe():
-    report_1 = [7, 6, 4, 2, 1]
-    report_2 = [1, 2, 7, 8, 9]
-    report_3 = [9, 7, 6, 2, 1]
-    report_4 = [1, 3, 2, 4, 5]
-    report_5 = [8, 6, 4, 4, 1]
-    report_6 = [1, 3, 6, 7, 9]
-    report_7 = [6, 4, 7, 8, 10, 11]
 
     assert determine_report_safe(report_1) is True
     assert determine_report_safe(report_2) is False
@@ -17,3 +21,11 @@ def test_determine_report_safe():
     assert determine_report_safe(report_5) is False
     assert determine_report_safe(report_6) is True
     assert determine_report_safe(report_7) is False
+
+
+def test_determnine_safe_after_dampening() -> None:
+    # Non included reports are already safe
+    assert determine_safe_after_dampening(report_2) is False
+    assert determine_safe_after_dampening(report_3) is False
+    assert determine_safe_after_dampening(report_4) is True
+    assert determine_safe_after_dampening(report_5) is True
