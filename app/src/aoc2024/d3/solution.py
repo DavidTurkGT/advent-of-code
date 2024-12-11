710116b74e3a15cfe6fb49f9653e4055397b392d
David Turk <david.turk@cfacorp.com>
2024-12-11 13:09:47 -0500

    Completes day 3 part 2

--- app/src/aoc2024/d3/solution.py
+++ app/src/aoc2024/d3/solution.py
@@ -7,16 +7,37 @@ def main():
 
     print(f"The sum of all mul operations in the corrupted memory is {sum}")
 
+    corrected_sum = calculate_sum_with_dos_and_donts(corrupted_memory)
+
+    print(
+        f"The corrected sum with do() and don't() taken into consideration is {corrected_sum}"
+    )
+
 
 def read_input_file() -> str:
     with open("./input.txt") as input_file:
-        input_rows = input_file.readlines()
-    return "".join(input_rows)
+        input_rows = input_file.read()
+    return input_rows
 
 
 def calculate_sum_of_muls_from_corrupted_memory(corrupted_memory_string: str) -> int:
     valid_mul_paramters = re.findall(r"mul\((\d+),(\d+)\)", corrupted_memory_string)
     return sum([int(x[0]) * int(x[1]) for x in valid_mul_paramters])
+
+
+def calculate_sum_with_dos_and_donts(corrupted_memory_string: str) -> int:
+    # Base case: if don't() is not in the string return total
+    if "don't()" not in corrupted_memory_string:
+        return calculate_sum_of_muls_from_corrupted_memory(corrupted_memory_string)
+
+    # Find the first case of don't(), everything to the left should be counted
+    i = corrupted_memory_string.find("don't()")
+    total = calculate_sum_of_muls_from_corrupted_memory(corrupted_memory_string[:i])
+    remaining_string = corrupted_memory_string[i:]
+
+    # With everything to the right, find the first case of do() and recurse on the right hand side
+    i = remaining_string.find("do()")
+    return total + calculate_sum_with_dos_and_donts(remaining_string[i:])
 
 
 if __name__ == "__main__":
--- app/test/aoc2024/test_2024d3_solution.py
+++ app/test/aoc2024/test_2024d3_solution.py
@@ -1,4 +1,7 @@
-from src.aoc2024.d3.solution import calculate_sum_of_muls_from_corrupted_memory
+from src.aoc2024.d3.solution import (
+    calculate_sum_of_muls_from_corrupted_memory,
+    calculate_sum_with_dos_and_donts,
+)
 
 
 def test_calculate_sum_of_muls_from_corrupted_memory() -> None:
@@ -7,3 +10,11 @@ def test_calculate_sum_of_muls_from_corrupted_memory() -> None:
     )
     sum = calculate_sum_of_muls_from_corrupted_memory(corrupted_memory)
     assert sum == 161
+
+
+def test_calculate_sum_with_dos_and_donts() -> None:
+    corrupted_memory = (
+        "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+    )
+    sum = calculate_sum_with_dos_and_donts(corrupted_memory)
+    assert sum == 48
